4383788161990fb9ed011989b19f8cfe00231789
Elizabeth Rosso <elizabeth@Elizabeths-MacBook-Air.local>
2018-10-01 19:35:55 -0400

    java scraper

---
+++ JavaScraper.java
@@ -0,0 +1,15 @@
+
+/**
+ * Scrapes Yahoo Finance
+ *
+ * @author Elizabeth Rosso
+ * @version 1.0
+ */
+public class JavaScraper {
+
+    	public static void main(String[] args) 
+	{
+        	// Prints "Hello, World" to the terminal window.
+        	System.out.println("Hello, World");
+	}   
+}
