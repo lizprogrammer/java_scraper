92857616e54303589694fbab70d0a5ff265c2721
Elizabeth Rosso <elizabeth@Elizabeths-MacBook-Air.local>
2018-10-02 21:48:17 -0400

    java scraper

--- JavaScraper.java
+++ JavaScraper.java
@@ -5,11 +5,40 @@
  * @author Elizabeth Rosso
  * @version 1.0
  */
+//package YahooStockReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class JavaScraper {
 
-    	public static void main(String[] args) 
+    	public static void main(String[] args) throws MalformedURLException, IOException, IllegalArgumentException
 	{
-        	// Prints "Hello, World" to the terminal window.
-        	System.out.println("Hello, World");
+			final String SYM = "KO";
+			URL url = new URL("https://finance.yahoo.com/quote/SONC?p=SONC");
+			
+			int MAXNUMBER = 1;
+
+			try{
+
+				URLConnection urlConn = url.openConnection();
+				InputStreamReader inStream = new InputStreamReader(urlConn.getInputStream());
+				BufferedReader buff = new BufferedReader(inStream);
+
+				String line = buff.readLine();
+				int lineNumber = 0;
+				while(lineNumber <= MAXNUMBER){
+				//while(line != null){
+					System.out.println(line);
+				} 
+			}catch (Exception e) {
+				throw e;
+			}	
+
+
+			
 	}   
 }
